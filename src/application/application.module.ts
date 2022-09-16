93b5a9542f9b24204f7d6aae06255aa141943870
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2022-09-17 00:47:10 +0500

    Application module file cleanup

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ClientRepository } from 'src/user-info/client/repository/client.repository';
 import { ApplicationController } from './application.controller';
 import { ApplicationRepository } from './repository/application.repository';
 import { ApplicationService } from './service/application.service';
@@ -8,7 +7,6 @@ import { ApplicationService } from './service/application.service';
 @Module({
   imports: [TypeOrmModule.forFeature([ApplicationRepository])],
   controllers: [ApplicationController],
-  // TODO: Try to remove client repository. Looks like we don't need it here
-  providers: [ApplicationService, ClientRepository],
+  providers: [ApplicationService],
 })
 export class ApplicationModule {}
